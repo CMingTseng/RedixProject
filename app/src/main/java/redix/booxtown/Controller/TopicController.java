88a7f8b46cccd1e25722ce1554c294963357d2d6
duong <dodangduong.it.1992@gmail.com>
2016-09-28 22:47:06 +0700

    sâsa

--- app/src/main/java/redix/booxtown/Controller/TopicController.java
+++ app/src/main/java/redix/booxtown/Controller/TopicController.java
@@ -43,6 +43,24 @@ public class TopicController {
         return null;
     }
 
+
+    public List<Topic> gettopicbyid(String id){
+        Call<TopicResult> getTopTopic = service.gettopicbyid(id);
+        try {
+            if (android.os.Build.VERSION.SDK_INT > 9) {
+                StrictMode.ThreadPolicy policy =
+                        new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                StrictMode.setThreadPolicy(policy);
+            }
+            TopicResult str = getTopTopic.execute().body();
+            if (str.getCode()==200){
+                return str.getTopic();
+            }
+        } catch (Exception ex) {
+        }
+        return null;
+    }
+
     public List<Topic> getALllTopicTop(String session_id, int top, int from){
         Call<TopicResult> getTopTopic = service.topic_gettop(session_id, top, from);
         try {
