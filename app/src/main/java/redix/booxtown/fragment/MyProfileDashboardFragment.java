299a187d38eec30ed2577f60ef08e04657243075
dodangduong <dodangduong.it.1992@gmail.com>
2016-10-03 00:00:09 +0700

    hahah aa

--- app/src/main/java/redix/booxtown/fragment/MyProfileDashboardFragment.java
+++ app/src/main/java/redix/booxtown/fragment/MyProfileDashboardFragment.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -19,6 +20,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import redix.booxtown.R;
@@ -33,35 +35,28 @@ public class MyProfileDashboardFragment extends Fragment {
     TextView txt_username;
     ListView lv_myprofile_dashboard;
     int user_id;
+    List<DashBoard> dashBoards_new;
+    AdapterProfileDashboard adapterProfileDashboard;
+    ImageView img_menu_component,img_menu,imv_close_dialog_dashboard_status,imageView26,imageView27;
+    TextView title_menu;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.my_profile_dashboard_fragment, container, false);
-        lv_myprofile_dashboard = (ListView)view.findViewById(R.id.lv_myprofile_dashboard);
-
-        ImageView img_menu_component = (ImageView)getActivity().findViewById(R.id.img_menu_component);
+        init(view);
+        dashBoards_new = new ArrayList<>();
         img_menu_component.setVisibility(View.GONE);
-
-        TextView title_menu = (TextView)getActivity().findViewById(R.id.txt_title);
         title_menu.setText("My Profile");
-
-        ImageView img_menu = (ImageView)getActivity().findViewById(R.id.img_menu);
         Picasso.with(getContext()).load(R.drawable.btn_sign_in_back).into(img_menu);
-
-        ImageView imv_close_dialog_dashboard_status = (ImageView)view.findViewById(R.id.imv_close_dialog_dashboard_status);
         Picasso.with(getContext()).load(R.drawable.btn_rank_one).into(imv_close_dialog_dashboard_status);
-
-        ImageView imageView26 = (ImageView)view.findViewById(R.id.imageView26);
         Picasso.with(getContext()).load(R.drawable.btn_rank_two).into(imageView26);
-
-        ImageView imageView27 = (ImageView)view.findViewById(R.id.imageView27);
         Picasso.with(getContext()).load(R.drawable.btn_rank_three).into(imageView27);
         SharedPreferences pref = getContext().getSharedPreferences("MyPref", Context.MODE_PRIVATE);
         final String session_id =  pref.getString("session_id", null);
         final String user_name = pref.getString("username",null);
         user_id = Integer.valueOf(pref.getString("user_id",null));
-        txt_username = (TextView)view.findViewById(R.id.txt_profile_username);
+
         txt_username.setText(user_name);
 
         img_menu.setOnClickListener(new View.OnClickListener() {
@@ -70,9 +65,37 @@ public class MyProfileDashboardFragment extends Fragment {
                 callFragment(new MyProfileFragment());
             }
         });
-        getDashBoard getDashBoard = new getDashBoard(getContext(),session_id,100,0);
-        getDashBoard.execute();
+//        getDashBoard getDashBoard = new getDashBoard(getContext(),session_id,5,0);
+//        getDashBoard.execute();
+
+        lv_myprofile_dashboard.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+
+            }
+            @Override
+            public void onScroll(AbsListView absListView, int i, int i1, int i2) {
+                int lastitem = i+i1;
+                if(lastitem == i2){
+                    getDashBoard getDashBoard = new getDashBoard(getContext(),session_id,lastitem+5,lastitem);
+                    getDashBoard.execute();
+                    adapterProfileDashboard.notifyDataSetChanged();
+                }
+            }
+        });
+
         return view;
+    }
+
+    public void init(View view){
+        txt_username = (TextView)view.findViewById(R.id.txt_profile_username);
+        imageView27 = (ImageView)view.findViewById(R.id.imageView27);
+        imv_close_dialog_dashboard_status = (ImageView)view.findViewById(R.id.imv_close_dialog_dashboard_status);
+        imageView26 = (ImageView)view.findViewById(R.id.imageView26);
+        img_menu = (ImageView)getActivity().findViewById(R.id.img_menu);
+        lv_myprofile_dashboard = (ListView)view.findViewById(R.id.lv_myprofile_dashboard);
+        img_menu_component = (ImageView)getActivity().findViewById(R.id.img_menu_component);
+        title_menu = (TextView)getActivity().findViewById(R.id.txt_title);
     }
 
     public void callFragment(Fragment fragment ){
@@ -115,19 +138,10 @@ public class MyProfileDashboardFragment extends Fragment {
         protected void onPostExecute(final List<DashBoard> dashBoards) {
             try {
                 if(dashBoards.size() > 0){
-                    final AdapterProfileDashboard adapterProfileDashboard = new AdapterProfileDashboard(getActivity(),dashBoards,user_id);
-//                    lv_myprofile_dashboard.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                        @Override
-//                        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                            if(i==0){
-//                                callFragment(new DashboardStatusFragment());
-//                            }else if(i==1) {
-//                                callFragment(new DashboardStopFragment());
-//                            }else if(i==2){
-//                                callFragment(new DashboardDeleteFragment());
-//                            }
-//                        }
-//                    });
+                    for (int i=0;i<dashBoards.size();i++){
+                        dashBoards_new.add(dashBoards.get(i));
+                    }
+                    adapterProfileDashboard = new AdapterProfileDashboard(getActivity(),dashBoards_new,user_id);
                     lv_myprofile_dashboard.setAdapter(adapterProfileDashboard);
                     lv_myprofile_dashboard.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
