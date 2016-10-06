07dde4a6b3a6171f3655f1d05596d75e9e566ad1
dodangduong <dodangduong.it.1992@gmail.com>
2016-10-07 01:08:40 +0700

    hahah aa

--- app/src/main/java/redix/booxtown/adapter/AdapterTopic.java
+++ app/src/main/java/redix/booxtown/adapter/AdapterTopic.java
@@ -80,7 +80,6 @@ public class AdapterTopic extends BaseAdapter {
 
             if (interact.getIs_read() == 0) {
                 holder.txt_count_interact.setTextColor(context.getResources().getColor(R.color.color_text));
-
             } else {
                 holder.txt_count_interact.setTextColor(context.getResources().getColor(R.color.color_topic_interact));
             }
